bdc7d22590f1b20898346e22618d88b0cd22b4fe
Saksham Khanal <saksham.khanal01@gmail.com>
2023-06-09 22:25:31 +0545

    A simple wikipedia scrapper

---
+++ Wikipedia_scrapper.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+page = requests.get("https://en.wikipedia.org/wiki/Main_Page")
+soup = BeautifulSoup(page.content,'html.parser')
+
+object = soup.find(id="mp-left")
+items = object.find_all(class_="mp-h2")
+result = items[0]
+
+print(result.prettify())
